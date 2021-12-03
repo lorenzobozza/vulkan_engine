cae73b3a2ceb9209c6385af3a3bc639b381162d9
Lorenzo Bozza <lorenzo.bozza.21@live.it>
2021-12-03 18:25:38 +0100

    CMake macOS

---
+++ build/CMakeFiles/vulkan_engine.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan_engine.dir/source/2bodyproblem.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/2bodyproblem.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Application.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Application.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Buffer.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Buffer.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Camera.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Camera.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Descriptors.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Descriptors.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Device.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Device.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Keyboard.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Keyboard.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Model.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Model.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Pipeline.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Pipeline.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/RenderSystem.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/RenderSystem.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Renderer.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Renderer.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/SolidObject.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/SolidObject.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/SwapChain.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/SwapChain.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/Window.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/Window.cpp.o.d"
+  "CMakeFiles/vulkan_engine.dir/source/main.cpp.o"
+  "CMakeFiles/vulkan_engine.dir/source/main.cpp.o.d"
+  "vulkan_engine"
+  "vulkan_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vulkan_engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vulkan_engine.
+# This may be replaced when dependencies are built.
