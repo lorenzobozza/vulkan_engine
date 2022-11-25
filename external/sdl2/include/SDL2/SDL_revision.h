73c8e5e73b01cccef0ab79d73ad50ca31859007e
Lorenzo Bozza <lorenzo.bozza.21@live.it>
2022-11-25 21:27:04 +0100

    SDL updated to 2.24.2

--- external/sdl2/include/SDL2/SDL_revision.h
+++ external/sdl2/include/SDL2/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@8c9beb0c873f6ca5efbd88f1ad2648bfc793b2ac"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@55b03c7493a7abed33cf803d1380a40fa8af903f"
 #define SDL_REVISION_NUMBER 0
