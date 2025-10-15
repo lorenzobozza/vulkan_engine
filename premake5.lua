6a9d28245e4c990bd352e7e9256fd741dec05e4a
Lorenzo Bozza <lorenzo.bozza.21@live.it>
2025-10-15 15:51:30 +0200

    Updated SDL2, Tiff and FT; fused all dylib as fat universal libs

--- premake5.lua
+++ premake5.lua
@@ -10,15 +10,16 @@ project "Core"
 	language "C++"
 	cppdialect "C++17"
 
-	defines { "SDL_DISABLE_IMMINTRIN_H" }
-
 	files {
 		"**.c",
 		"**.cpp"
 	}
 
 	includedirs {
-		"source/include",
+		"source/include"
+	}
+
+	externalincludedirs {
 		"external/*",
 		"external/utils/*",
 		"external/**/include"
@@ -28,12 +29,12 @@ project "Core"
 		"vulkan.1.3.236",
 		"SDL2-2.0.0",
 		"SDL2_image-2.0.0",
-		"tiff-4.5.0",
+		"tiff.6",
 		"freetype.6"
 	}
 
 	libdirs {
-		"external/**/lib/**"
+		"external/**/lib"
 	}
 
 	filter "configurations:Debug"
@@ -43,8 +44,3 @@ project "Core"
 	filter "configurations:Release"
 		defines { "NDEBUG" }
 		optimize "On"
-
-	filter "action:xcode4"
-		xcodebuildsettings {
-			["OTHER_LDFLAGS"] = "-llibvulkan.1.3.236.dylib -llibSDL2-2.0.0.dylib -llibSDL2_image-2.0.0.dylib -llibtiff-4.5.0.dylib -llibfreetype.6.dylib"
-		}
